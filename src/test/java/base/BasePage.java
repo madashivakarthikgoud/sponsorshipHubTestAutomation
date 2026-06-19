1a9d0d47d7cb998e9b2a1375f3ae844c5dfaa326
Shiva Karthik <madashivakarthikgoud@gmail.com>
2026-06-19 18:12:20 +0530

    feat: add BasePage and BaseTest

--- src/test/java/base/BasePage.java
+++ src/test/java/base/BasePage.java
@@ -1,4 +1,19 @@
 package base;
 
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.time.Duration;
+
 public class BasePage {
+    public WebDriver driver;
+    public Wait<WebDriver> wait;
+    public BasePage(WebDriver driver){
+        this.driver=driver;
+        wait=new FluentWait<>(driver).withTimeout(Duration.ofSeconds(15)).pollingEvery(Duration.ofSeconds(1)).ignoring(NoSuchElementException.class);
+        PageFactory.initElements(driver,this);
+    }
 }
--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,4 +1,22 @@
 package base;
 
-public class BaseTest {
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.testng.annotations.BeforeTest;
+
+import java.time.Duration;
+
+public class BaseTest{
+    public WebDriver driver;
+    public Wait<WebDriver> wait;
+    @BeforeTest
+    public void initializer(){
+        driver = new ChromeDriver();
+        wait = new FluentWait<>(driver).withTimeout(Duration.ofSeconds(30)).pollingEvery(Duration.ofSeconds(1)).ignoring(NoSuchElementException.class);
+        driver.get("https://sponsorship-front.netlify.app/login");
+        driver.manage().window().maximize();
+    }
 }
